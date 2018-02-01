7397eb915d161416305d02d90f5062337d1a1416
dimaSkalora <timon2@ukr.net>
2018-02-01 18:49:35 +0200

    model_01

---
+++ src/main/java/com/online/shelter/pet/servlet/model/Animal.java
@@ -0,0 +1,100 @@
+package com.online.shelter.pet.servlet.model;
+
+import java.time.LocalDateTime;
+
+public class Animal {
+    private final LocalDateTime dateTime;
+    private final String typeAnimal;
+    private final String nameAnimal;
+    private final String breed;
+    private final char sex;
+    private final String color;
+    private final double age;
+    private final double growth;
+    private final double weight;
+
+    private final String namePerson;
+    private final String phone;
+    private final String email;
+
+    public Animal(LocalDateTime dateTime, String typeAnimal, String nameAnimal, String breed, char sex, String color, double age, double growth, double weight, String namePerson, String phone, String email) {
+        this.dateTime = dateTime;
+        this.typeAnimal = typeAnimal;
+        this.nameAnimal = nameAnimal;
+        this.breed = breed;
+        this.sex = sex;
+        this.color = color;
+        this.age = age;
+        this.growth = growth;
+        this.weight = weight;
+        this.namePerson = namePerson;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getTypeAnimal() {
+        return typeAnimal;
+    }
+
+    public String getNameAnimal() {
+        return nameAnimal;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public double getGrowth() {
+        return growth;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getNamePerson() {
+        return namePerson;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "dateTime=" + dateTime +
+                ", typeAnimal='" + typeAnimal + '\'' +
+                ", nameAnimal='" + nameAnimal + '\'' +
+                ", breed='" + breed + '\'' +
+                ", sex=" + sex +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                ", growth=" + growth +
+                ", weight=" + weight +
+                ", namePerson='" + namePerson + '\'' +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/online/shelter/pet/servlet/model/AnimalWithDownplayWeight.java
@@ -0,0 +1,56 @@
+package com.online.shelter.pet.servlet.model;
+
+import java.time.LocalDateTime;
+
+public class AnimalWithDownplayWeight {
+    private final LocalDateTime dateTime;
+    private final String typeAnimal;
+    private final String nameAnimal;
+    private final String breed;
+    private final char sex;
+    private final String color;
+    private final double age;
+    private final double growth;
+    private final double weight;
+
+    private final String namePerson;
+    private final String phone;
+    private final String email;
+
+    private final boolean downplayWeight;
+
+    public AnimalWithDownplayWeight(LocalDateTime dateTime, String typeAnimal, String nameAnimal, String breed, char sex, String color, double age, double growth, double weight, String namePerson, String phone, String email, boolean downplayWeight) {
+        this.dateTime = dateTime;
+        this.typeAnimal = typeAnimal;
+        this.nameAnimal = nameAnimal;
+        this.breed = breed;
+        this.sex = sex;
+        this.color = color;
+        this.age = age;
+        this.growth = growth;
+        this.weight = weight;
+        this.namePerson = namePerson;
+        this.phone = phone;
+        this.email = email;
+        this.downplayWeight = downplayWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "AnimalWithDownplayWeight{" +
+                "dateTime=" + dateTime +
+                ", typeAnimal='" + typeAnimal + '\'' +
+                ", nameAnimal='" + nameAnimal + '\'' +
+                ", breed='" + breed + '\'' +
+                ", sex=" + sex +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                ", growth=" + growth +
+                ", weight=" + weight +
+                ", namePerson='" + namePerson + '\'' +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                ", downplayWeight=" + downplayWeight +
+                '}';
+    }
+}
