9dee2f364523a2e129c811f89747ea7c55b13d55
dimaSkalora <timon2@ukr.net>
2018-02-13 14:14:21 +0200

    servlet - 05. update hsqldb

--- src/main/java/com/online/shelter/pet/servlet/repository/jdbc/JdbcPetRepositoryImpl.java
+++ src/main/java/com/online/shelter/pet/servlet/repository/jdbc/JdbcPetRepositoryImpl.java
@@ -1,10 +1,8 @@
 package com.online.shelter.pet.servlet.repository.jdbc;
 
-import com.online.shelter.pet.servlet.Profiles;
 import com.online.shelter.pet.servlet.model.Pet;
 import com.online.shelter.pet.servlet.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,54 +13,31 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public abstract class JdbcPetRepositoryImpl<T> implements PetRepository {
+public class JdbcPetRepositoryImpl implements PetRepository {
     private static final RowMapper<Pet> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Pet.class);
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final SimpleJdbcInsert insertPet;
 
     @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    private SimpleJdbcInsert insertPet;
-
-    protected abstract T toDbDateTime(LocalDateTime ldt);
-
-    @Repository
-    @Profile(Profiles.POSTGRES_DB)
-    public static class Java8JdbcMealRepositoryImpl extends JdbcPetRepositoryImpl<LocalDateTime> {
-        @Override
-        protected LocalDateTime toDbDateTime(LocalDateTime ldt) {
-            return ldt;
-        }
-    }
-
-    @Repository
-    @Profile(Profiles.HSQL_DB)
-    public static class TimestampJdbcMealRepositoryImpl extends JdbcPetRepositoryImpl<Timestamp> {
-        @Override
-        protected Timestamp toDbDateTime(LocalDateTime ldt) {
-            return Timestamp.valueOf(ldt);
-        }
-    }
-
-    @Autowired
-    private void setDataSource(DataSource dataSource) {
+    public JdbcPetRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.insertPet = new SimpleJdbcInsert(dataSource)
                 .withTableName("pets")
                 .usingGeneratedKeyColumns("id");
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     @Override
     public Pet save(Pet pet, int userId) {
         MapSqlParameterSource map = new MapSqlParameterSource()
                 .addValue("id", pet.getId())
-                .addValue("createdDate", toDbDateTime(pet.getCreatedDate()))
+                .addValue("createdDate", pet.getCreatedDate())
                 .addValue("typePet", pet.getTypePet())
                 .addValue("namePet", pet.getNamePet())
                 .addValue("breed", pet.getBreed())
@@ -113,6 +88,6 @@ public abstract class JdbcPetRepositoryImpl<T> implements PetRepository {
     public List<Pet> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM pets WHERE user_id=?  AND createddate BETWEEN  ? AND ? ORDER BY createddate DESC",
-                ROW_MAPPER, userId, toDbDateTime(startDate), toDbDateTime(endDate));
+                ROW_MAPPER, userId, startDate, endDate);
     }
 }
