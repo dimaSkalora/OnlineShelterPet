89fa5d524a64ba0a9945b4ff6ea2c87b984845dc
dimaSkalora <timon2@ukr.net>
2018-02-13 10:26:32 +0200

    servlet - 05. DataJpaPetRepositoryImpl

--- src/main/java/com/online/shelter/pet/servlet/repository/datajpa/DataJpaPetRepositoryImpl.java
+++ src/main/java/com/online/shelter/pet/servlet/repository/datajpa/DataJpaPetRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.online.shelter.pet.servlet.model.Pet;
 import com.online.shelter.pet.servlet.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,28 +15,35 @@ public class DataJpaPetRepositoryImpl implements PetRepository {
     @Autowired
     private CrudPetRepository crudPetRepository;
 
+    @Autowired
+    private CrudUserRepository crudUserRepository;
+
     @Override
+    @Transactional
     public Pet save(Pet pet, int userId) {
-        return null;
+        if(!pet.isNew() && get(pet.getId(),userId) == null)
+            return null;
+        pet.setUser(crudUserRepository.getOne(userId));
+        return crudPetRepository.save(pet);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudPetRepository.delete(id,userId) != 0;
     }
 
     @Override
     public Pet get(int id, int userId) {
-        return null;
+        return crudPetRepository.findById(id).filter(pet->pet.getUser().getId() == userId).orElse(null);
     }
 
     @Override
     public List<Pet> getAll(int userId) {
-        return null;
+        return crudPetRepository.getAll(userId);
     }
 
     @Override
     public List<Pet> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudPetRepository.getBetween(startDate, endDate, userId);
     }
 }
