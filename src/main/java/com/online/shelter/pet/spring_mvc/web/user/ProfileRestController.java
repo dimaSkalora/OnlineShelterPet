f91cd5bc403e2bd9c81aea472491297484423862
dimaSkalora <timon2@ukr.net>
2018-02-16 16:55:42 +0200

    SpringMVC - 07. rest controller

--- src/main/java/com/online/shelter/pet/spring_mvc/web/user/ProfileRestController.java
+++ src/main/java/com/online/shelter/pet/spring_mvc/web/user/ProfileRestController.java
@@ -2,20 +2,26 @@ package com.online.shelter.pet.spring_mvc.web.user;
 
 import com.online.shelter.pet.spring_mvc.AuthorizedUser;
 import com.online.shelter.pet.spring_mvc.model.User;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
 public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get() {
         return super.get(AuthorizedUser.id());
     }
 
+    @DeleteMapping
     public void delete() {
         super.delete(AuthorizedUser.id());
     }
 
-    public void update(User user) {
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody User user) {
         super.update(user, AuthorizedUser.id());
     }
 }
