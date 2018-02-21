308ca01235b93c377fda6da37849c0d6c6371468
dimaSkalora <timon2@ukr.net>
2018-02-21 12:56:11 +0200

    SpringMVC - 09. js i18n

--- src/main/webApp/SpringMVC/resources/js/datatablesUtil.js
+++ src/main/webApp/SpringMVC/resources/js/datatablesUtil.js
@@ -10,7 +10,7 @@ function makeEditable() {
 }
 
 function add() {
-    form.find(":input").val("");
+    $("#modalTitle").html(i18n["addTitle"]);
     $("#editRow").modal();
 }
 
@@ -29,7 +29,7 @@ function deleteRow(id) {
         type: "DELETE"
     }).done(function () {
         updateTable();
-        successNoty("Deleted");
+        successNoty("common.deleted");
     });
 }
 
@@ -45,7 +45,7 @@ function save() {
     }).done(function () {
         $("#editRow").modal("hide");
         updateTable();
-        successNoty("Saved");
+        successNoty("common.saved");
     });
 }
 
@@ -59,10 +59,10 @@ function closeNoty() {
     }
 }
 
-function successNoty(text) {
+function successNoty(key) {
     closeNoty();
     new Noty({
-        text: "<span class='glyphicon glyphicon-ok'></span> &nbsp;" + text,
+        text: "<span class='glyphicon glyphicon-ok'></span> &nbsp;" + i18n[key],
         type: 'success',
         layout: "bottomRight",
         timeout: 1000
@@ -72,7 +72,7 @@ function successNoty(text) {
 function failNoty(jqXHR) {
     closeNoty();
     failedNote = new Noty({
-        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;Error status: " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
+        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
         type: "error",
         layout: "bottomRight"
     }).show();
