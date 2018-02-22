f1edc38cc74469d3e5e4305b50c379ce1bb51e3b
dimaSkalora <timon2@ukr.net>
2018-02-22 16:43:13 +0200

    SpringMVC - 10. secure tag annotation

--- src/main/java/com/online/shelter/pet/spring_mvc/web/RootController.java
+++ src/main/java/com/online/shelter/pet/spring_mvc/web/RootController.java
@@ -5,6 +5,7 @@ import com.online.shelter.pet.spring_mvc.service.PetService;
 import com.online.shelter.pet.spring_mvc.service.UserService;
 import com.online.shelter.pet.spring_mvc.util.PetsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,8 @@ public class RootController {
         return "redirect:pets";
     }
 
+    //    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/users")
     public String users() {
         return "users";
